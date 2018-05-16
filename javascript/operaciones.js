22b63f7ab68bff53e58650f79f41c53f6f384d23
CamiloMR <camilo98.2254@gmail.com>
2018-05-16 17:39:32 -0500

    Versión 2.6

--- javascript/operaciones.js
+++ javascript/operaciones.js
@@ -1,3 +1,7 @@
+var http = required("http");
+var express = required("express");
+var app = express();
+
 function obtenerParametros(ev){
   var nomProduct = document.getElementById("inputNombreProducto");
   var nomCliente = document.getElementById("inputNombreCliente");
@@ -17,7 +21,27 @@ function obtenerParametros(ev){
 
   var toJSON = JSON.stringify(obj);
 
-  var spanJ = getElementById("spanJSON");
-  spanJ.textContent = toJSON;
-  var text = spanJSON.textContent;
+  document.getElementById("spanJSON").textContent = toJSON;
+}
+
+var lista = new Array();
+
+function enviar_mensaje(req, res){
+   res.render('../compra', {
+      lista: lista
+   });
+}
+
+exports.get_enviar_mensaje = function(req, res){
+   enviar_mensaje(req, res);
+}
+
+exports.post_enviar_mensaje = function(req, res){
+   var asunto = req.body.asunto;
+   var mensaje = req.body.mensaje;
+   lista.push({
+      asunto: asunto,
+      mensaje: mensaje
+   })
+   enviar_mensaje(req, res);
 }
